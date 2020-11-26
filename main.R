d64c719f3066286d1d8c52d9aa6c5e6ffe1c49a7
Adam Kapica <adam.kapica@gmail.com>
2020-11-26 14:04:24 +0100

    Rozpisanie podzadan do wykonania.

--- main.R
+++ main.R
@@ -1,7 +1,26 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: adam
+# Title     : Tree Augmented Naive Bayes (TAN)
 # Created on: 26.11.2020
 
-print(sample(1:3))
-print(sample(c(2,5,3), size=4, replace=TRUE))
+file_list <- c("data/cmc.csv", "data/diabetes.csv", "data/occupancy.csv", "data/wine.csv", "data/zoo.csv")
+curr_file <- file_list[1]
+
+data <- read.csv(file = curr_file, header = FALSE)
+head(data)
+tail(data)
+
+# podzial zbioru na treningowy/testowy, chwilowo mozna operowac na calosci
+
+# stworzenie macierzy liczba_atrybutow x liczba_atrybutow
+# wyliczenie informacji wzajemnej miedzy atrybutami i wpisanie do macierzy
+# posortowanie wszystkich trojek (Atrybut x Atrybut x Informacja wzajemna miedzy nimi)
+# wyznaczenie drzewa rozpinajacego - (liczba_atrybutow -1) krawedzi (Atrybut x Atrybut x Informacja)
+
+# tu nie jestem pewien:
+# wyliczenie prawdopodobienstw warunkowych dla kazdego wezla z wykorzystaniem wygladzenia Laplace'a
+# predykcja przy wykorzystaniu drzewa
+
+# import NB pakiet e1071 dzialajacego dla plikow csv
+# import Drzewa Decyzyjnego pakiet party dzialajacego dla plikow csv
+
+# funkcja testujaca i zapisujaca wyniki
+# funkcja podsumowujaca wyniki eksperymentow
