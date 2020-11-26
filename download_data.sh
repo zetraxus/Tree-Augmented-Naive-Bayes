719f9c34fb49b4a2b0ad2b81611f46127736072a
Adam Kapica <adam.kapica@gmail.com>
2020-11-26 13:29:50 +0100

    upgrade download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,20 @@
+# prepare dir
 rm -rf data/*
-mkdir data/raw
-wget -O data/raw/cmc.data https://archive.ics.uci.edu/ml/machine-learning-databases/cmc/cmc.data
-wget -O data/raw/wine.csv https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv
-wget -O data/raw/occupancy.zip https://archive.ics.uci.edu/ml/machine-learning-databases/00357/occupancy_data.zip
-wget -O data/raw/zoo.data https://archive.ics.uci.edu/ml/machine-learning-databases/zoo/zoo.data
-wget -O data/raw/diabetes.csv http://nrvis.com/data/mldata/pima-indians-diabetes.csv
+mkdir -p data
+
+# download data
+wget -O data/cmc.csv https://archive.ics.uci.edu/ml/machine-learning-databases/cmc/cmc.data
+wget -O data/wine.csv https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv
+wget -O data/occupancy.zip https://archive.ics.uci.edu/ml/machine-learning-databases/00357/occupancy_data.zip
+wget -O data/zoo.csv https://archive.ics.uci.edu/ml/machine-learning-databases/zoo/zoo.data
+wget -O data/diabetes.csv http://nrvis.com/data/mldata/pima-indians-diabetes.csv
+
+# prepare data
+sed -i '1d' data/wine.csv
+sed -i 's/;/,/g' data/wine.csv
+
+unzip data/occupancy.zip -d data/
+cat data/*.txt >> data/occupancy.csv
+sed -i '1d' data/occupancy.csv
+rm data/*.txt
+rm data/occupancy.zip
