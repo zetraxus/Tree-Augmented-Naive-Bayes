2197a652d542910514c0cd1fe2e882ef61037e56
tomsinho <t.wiaderwek@gmail.com>
2020-11-27 20:07:18 +0100

    Calculate Conditional Mutual Information

--- main.R
+++ main.R
@@ -1,6 +1,9 @@
 # Title     : Tree Augmented Naive Bayes (TAN)
 # Created on: 26.11.2020
 
+install.packages("infotheo")
+library(infotheo)
+
 file_list <- c("data/cmc.csv", "data/diabetes.csv", "data/occupancy.csv", "data/wine.csv", "data/zoo.csv")
 curr_file <- file_list[1]
 
@@ -12,6 +15,24 @@ tail(data)
 
 # stworzenie macierzy liczba_atrybutow x liczba_atrybutow
 # wyliczenie informacji wzajemnej miedzy atrybutami i wpisanie do macierzy
+
+getConditionalMutualInformation <- function(attributes, class) {
+    mutualInformations <- data.frame(matrix(ncol = 3, nrow = 0))
+    columns <- c("I", "atr1", "atr2")
+    colnames(mutualInformations) <- columns
+    for (i in 1:(ncol(attributes) - 1)) {
+        for (j in i:ncol(attributes)) {
+            mutualInformation <- data.frame(condinformation(attributes[,i], attributes[,j], class), i, j)
+            colnames(mutualInformation) <- columns
+            mutualInformations <- rbind(mutualInformations, mutualInformation)
+        }
+    }
+
+    return(mutualInformations)
+}
+
+I <- getConditionalMutualInformation(data[,1:9], data[,10])
+head(I)
 # posortowanie wszystkich trojek (Atrybut x Atrybut x Informacja wzajemna miedzy nimi)
 # wyznaczenie drzewa rozpinajacego - (liczba_atrybutow -1) krawedzi (Atrybut x Atrybut x Informacja)
 
